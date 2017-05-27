4ff2c32f6162656689d67dbcffd7ec5dd5980362
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-28 03:10:44 +0430

    Build app structure

--- notes/app/src/androidTest/java/ir/coursio/notes/ExampleInstrumentedTest.java
+++ notes/app/src/androidTest/java/ir/coursio/notes/ExampleInstrumentedTest.java
@@ -18,7 +18,7 @@ import static org.junit.Assert.*;
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
+        // Context of the App under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("ir.coursio.notes", appContext.getPackageName());
