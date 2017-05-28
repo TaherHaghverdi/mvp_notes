c8d3c3bd5ef151d808aeecb28f625b4a061265eb
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-28 10:38:17 +0430

    Folders presenter updatep

--- notes/app/src/main/java/ir/coursio/notes/presenter/FoldersPresenter.java
+++ notes/app/src/main/java/ir/coursio/notes/presenter/FoldersPresenter.java
@@ -1,9 +1,22 @@
 package ir.coursio.notes.presenter;
 
+import ir.coursio.notes.component.FoldersComponent;
+
 /**
  * Created by Taher on 28/05/2017.
  * Project: notes
  */
 
-public class FoldersPresenter {
+public class FoldersPresenter implements FoldersComponent.Presenter {
+
+    private FoldersComponent.View view;
+
+    public FoldersPresenter(FoldersComponent.View view){
+        this.view = view;
+    }
+
+    @Override
+    public void present() {
+
+    }
 }
