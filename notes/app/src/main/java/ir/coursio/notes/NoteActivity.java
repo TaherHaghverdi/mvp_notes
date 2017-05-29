80842501f18be4c3a403472ec872aa8466f96213
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-29 11:30:34 +0430

    Added presenter to NoteActivity

--- notes/app/src/main/java/ir/coursio/notes/NoteActivity.java
+++ notes/app/src/main/java/ir/coursio/notes/NoteActivity.java
@@ -3,6 +3,8 @@ package ir.coursio.notes;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ir.coursio.notes.model.NoteModel;
+import ir.coursio.notes.presenter.NotePresenter;
 import ir.coursio.notes.view.NoteView;
 
 /**
@@ -11,16 +13,30 @@ import ir.coursio.notes.view.NoteView;
  */
 
 public class NoteActivity extends AppCompatActivity {
+
     public static final String FOLDER_ID = "folder_id";
+
     NoteView view;
+    NotePresenter presenter;
+    NoteModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //model = new NoteModel(this);
         view = new NoteView(this);
+
         setContentView(view);
+
+        presenter = new NotePresenter(view, model);
+        presenter.onCreate();
 
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        presenter.onDestroy();
+    }
 }
--- notes/app/src/main/java/ir/coursio/notes/presenter/NotePresenter.java
+++ notes/app/src/main/java/ir/coursio/notes/presenter/NotePresenter.java
@@ -1,9 +1,27 @@
 package ir.coursio.notes.presenter;
 
+import ir.coursio.notes.model.NoteModel;
+import ir.coursio.notes.view.NoteView;
+
 /**
  * Created by Taher on 29/05/2017.
  * Project: notes
  */
 
 public class NotePresenter {
+
+    private final NoteView view;
+    private final NoteModel model;
+
+    public NotePresenter(NoteView view, NoteModel model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void onCreate() {
+        //view.setPresenter(this);
+    }
+
+    public void onDestroy() {
+    }
 }
