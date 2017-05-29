0643999ec375e8d40ee1406ef955c6f0f22cd505
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-29 11:23:25 +0430

    Connected view to NoteActivity.

--- notes/app/src/main/java/ir/coursio/notes/NoteActivity.java
+++ notes/app/src/main/java/ir/coursio/notes/NoteActivity.java
@@ -1,6 +1,9 @@
 package ir.coursio.notes;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import ir.coursio.notes.view.NoteView;
 
 /**
  * Created by Taher on 28/05/2017.
@@ -9,5 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 
 public class NoteActivity extends AppCompatActivity {
     public static final String FOLDER_ID = "folder_id";
+    NoteView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        view = new NoteView(this);
+        setContentView(view);
+
+    }
 
 }
--- notes/app/src/main/java/ir/coursio/notes/view/NoteView.java
+++ notes/app/src/main/java/ir/coursio/notes/view/NoteView.java
@@ -4,7 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import ir.coursio.notes.R;
@@ -16,9 +19,18 @@ import ir.coursio.notes.R;
 @SuppressLint("ViewConstructor")
 public class NoteView extends FrameLayout {
 
+    ViewGroup mainLayout;
+
     public NoteView(@NonNull Activity activity) {
         super(activity);
-        View view = inflate(getContext(), R.layout.activity_main, this);
+        View view = inflate(getContext(), R.layout.activity_note, this);
 
+        mainLayout = (ViewGroup) view.findViewById(R.id.mainLayout);
+        FloatingActionButton fabAddNote = (FloatingActionButton) view.findViewById(R.id.fabAddNote);
+
+    }
+
+    public void showMessage(String message) {
+        Snackbar.make(mainLayout, message, Snackbar.LENGTH_SHORT).show();
     }
 }
