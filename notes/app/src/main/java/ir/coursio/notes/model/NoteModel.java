01dfde0dd62b247a4022183b55abc2820951804c
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-29 11:24:49 +0430

    Designed NoteActivity MVP pattern.

---
+++ notes/app/src/main/java/ir/coursio/notes/model/NoteModel.java
@@ -0,0 +1,9 @@
+package ir.coursio.notes.model;
+
+/**
+ * Created by Taher on 29/05/2017.
+ * Project: notes
+ */
+
+public class NoteModel {
+}
---
+++ notes/app/src/main/java/ir/coursio/notes/presenter/NotePresenter.java
@@ -0,0 +1,9 @@
+package ir.coursio.notes.presenter;
+
+/**
+ * Created by Taher on 29/05/2017.
+ * Project: notes
+ */
+
+public class NotePresenter {
+}
