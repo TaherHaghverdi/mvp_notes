a42fce5bb8ef88598963c28a95b61a16a3157ebe
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-29 12:35:23 +0430

    NoteAdapter.java is an adapter for notes list.

--- notes/app/src/main/java/ir/coursio/notes/view/adapter/NoteAdapter.java
+++ notes/app/src/main/java/ir/coursio/notes/view/adapter/NoteAdapter.java
@@ -1,11 +1,18 @@
 package ir.coursio.notes.view.adapter;
 
 import android.content.Context;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import ir.coursio.notes.R;
 import ir.coursio.notes.model.structures.NoteStruct;
 
 /**
@@ -14,22 +21,29 @@ import ir.coursio.notes.model.structures.NoteStruct;
  */
 
 public class NoteAdapter extends RecyclerView.Adapter {
-    Context context;
-    ArrayList<NoteStruct> notes;
+    private Context context;
+    private ArrayList<NoteStruct> notes;
+    private Listener listener;
 
-    public NoteAdapter(Context context, ArrayList<NoteStruct> notes) {
+
+    public NoteAdapter(Context context, ArrayList<NoteStruct> notes, Listener listener) {
         this.context = context;
         this.notes = notes;
+        this.listener = listener;
     }
 
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.menu_note, parent, false);
+        return new ViewHolder(view);
+
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        ViewHolder viewHolder = (ViewHolder) holder;
+        viewHolder.bindView(notes.get(position));
 
     }
 
@@ -43,7 +57,7 @@ public class NoteAdapter extends RecyclerView.Adapter {
     }
 
     public interface Listener {
-        void onNoteClicked(NoteStruct note);
+        void onNoteSelect(NoteStruct note);
     }
 
 
@@ -53,4 +67,47 @@ public class NoteAdapter extends RecyclerView.Adapter {
         notifyDataSetChanged();
     }
 
+    private class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView txtTitle, txtText;
+        private ImageView imgId;
+        private View view;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.view = view;
+            txtTitle = (TextView) view.findViewById(R.id.txtNoteTitle);
+            txtText = (TextView) view.findViewById(R.id.txtNoteText);
+            imgId = (ImageView) view.findViewById(R.id.txtCancel);
+        }
+
+
+        public void bindView(final NoteStruct note) {
+            txtTitle.setText(note.getTitle());
+
+            if (note.getDrawing() == null) {
+                setImageDrawable(R.drawable.ic_note_24dp, imgId);
+                txtText.setText(note.getText());
+            } else {
+                setImageDrawable(R.drawable.ic_image_24dp, imgId);
+                txtText.setVisibility(View.GONE);
+            }
+
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //  listener.onNoteClicked(note);
+                }
+            });
+        }
+    }
+
+    private void setImageDrawable(int id, ImageView img) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            img.setImageDrawable(context.getDrawable(id));
+        } else {
+            img.setImageDrawable(ContextCompat.getDrawable(context, id));
+        }
+
+    }
 }
