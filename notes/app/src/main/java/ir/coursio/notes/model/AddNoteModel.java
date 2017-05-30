59d3d403645694a398068f1718be45cb693344fa
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 12:27:20 +0430

    AddNoteModel base code

--- notes/app/src/main/java/ir/coursio/notes/model/AddNoteModel.java
+++ notes/app/src/main/java/ir/coursio/notes/model/AddNoteModel.java
@@ -1,9 +1,16 @@
 package ir.coursio.notes.model;
 
+import android.app.Activity;
+
 /**
  * Created by Taher on 30/05/2017.
  * Project: notes
  */
 
 public class AddNoteModel {
+    private final Activity activity;
+
+    public AddNoteModel(Activity activity) {
+        this.activity = activity;
+    }
 }
