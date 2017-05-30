550d509cf23c59ae0e4ef09c5338be5f8241cba2
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 12:26:16 +0430

    AddNotePresenter base code

--- notes/app/src/main/java/ir/coursio/notes/presenter/AddNotePresenter.java
+++ notes/app/src/main/java/ir/coursio/notes/presenter/AddNotePresenter.java
@@ -1,9 +1,26 @@
 package ir.coursio.notes.presenter;
 
+import ir.coursio.notes.model.AddNoteModel;
+import ir.coursio.notes.view.AddNoteView;
+
 /**
  * Created by Taher on 30/05/2017.
  * Project: notes
  */
 
 public class AddNotePresenter {
+
+    private final AddNoteView view;
+    private final AddNoteModel model;
+
+    public AddNotePresenter(AddNoteView view, AddNoteModel model) {
+        this.view = view;
+        this.model = model;
+    }
+
+    public void onCreate() {
+    }
+
+    public void onDestroy() {
+    }
 }
