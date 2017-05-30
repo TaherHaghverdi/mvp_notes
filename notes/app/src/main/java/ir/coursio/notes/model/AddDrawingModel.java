015e07fae8da9c3557f79d2ca6f56aafce3befe6
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 20:12:39 +0430

    Add saveDrawing method to save user painting in database

--- notes/app/src/main/java/ir/coursio/notes/model/AddDrawingModel.java
+++ notes/app/src/main/java/ir/coursio/notes/model/AddDrawingModel.java
@@ -1,6 +1,16 @@
 package ir.coursio.notes.model;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import java.io.ByteArrayOutputStream;
+
+import ir.coursio.notes.App;
+import ir.coursio.notes.model.db.DataContract;
+import ir.coursio.notes.model.structures.NoteStruct;
+import ir.coursio.notes.view.custom.DrawingView;
 
 /**
  * Created by Taher on 30/05/2017.
@@ -10,11 +20,47 @@ import android.app.Activity;
 public class AddDrawingModel {
     private final Activity activity;
 
+    public static final String NOTE = "note";
+    public static final String FOLDER_ID = "folder_id";
+    public static final String IS_EDITING = "is_editing";
+
     public AddDrawingModel(Activity activity) {
         this.activity = activity;
     }
 
-    public void saveDrawing(){
+    /**
+     * This method gets user drawing as a bitmap and covert it to stream of bytes.
+     * Then store them as an array of bytes.
+     * If isEditing is true means user is editing an existing note otherwise user is making new note.
+     *
+     * @param title       string that is title of drawing note
+     * @param drawingView the view that contains user drawing input
+     */
+    public void saveDrawing(String title, DrawingView drawingView) {
+        drawingView.setPaintXfermode();
+        drawingView.buildDrawingCache();
+        Bitmap signature = drawingView.getDrawingCache();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        signature.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+
+
+        ContentValues values = new ContentValues();
+        values.put(DataContract.NoteEntry.COLUMN_NOTE_TITLE, title);
+        values.put(DataContract.NoteEntry.COLUMN_NOTE_DRAW, byteArray);
+        values.put(DataContract.NoteEntry.COLUMN_FOLDER_ID, activity.getIntent().getStringExtra(FOLDER_ID));
+
+        if (activity.getIntent().getBooleanExtra(IS_EDITING, false))
+            App.getAppContext().getContentResolver().
+                    update(Uri.withAppendedPath(DataContract.FoldersEntry.CONTENT_URI_NOTES
+                            , activity.getIntent().getStringExtra(FOLDER_ID)), values, DataContract.NoteEntry._ID + "=" + ((NoteStruct) activity.getIntent().getParcelableExtra(NOTE)).getId(), null);
+        else
+            App.getAppContext().getContentResolver().insert(Uri.withAppendedPath(DataContract.FoldersEntry.CONTENT_URI_NOTES
+                    , activity.getIntent().getStringExtra(FOLDER_ID)), values);
+
+        signature.recycle();
+        drawingView.destroyDrawingCache();
+
 
     }
 }
