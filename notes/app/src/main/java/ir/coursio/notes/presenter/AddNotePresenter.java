0fd0fe3f7d256d7e0cafd907c141b3209dc7b67b
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 11:49:29 +0430

    Add new note MVP structure created

---
+++ notes/app/src/main/java/ir/coursio/notes/presenter/AddNotePresenter.java
@@ -0,0 +1,9 @@
+package ir.coursio.notes.presenter;
+
+/**
+ * Created by Taher on 30/05/2017.
+ * Project: notes
+ */
+
+public class AddNotePresenter {
+}
---
+++ notes/app/src/main/java/ir/coursio/notes/view/AddNoteView.java
@@ -0,0 +1,9 @@
+package ir.coursio.notes.view;
+
+/**
+ * Created by Taher on 30/05/2017.
+ * Project: notes
+ */
+
+public class AddNoteView {
+}
