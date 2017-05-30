0698fbc8a006474e59bfe51927647f804a04e5d6
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 12:24:29 +0430

    AddNoteActivity base code

--- notes/app/src/main/java/ir/coursio/notes/AddNoteActivity.java
+++ notes/app/src/main/java/ir/coursio/notes/AddNoteActivity.java
@@ -1,9 +1,31 @@
 package ir.coursio.notes;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import ir.coursio.notes.model.AddNoteModel;
+import ir.coursio.notes.presenter.AddNotePresenter;
+import ir.coursio.notes.view.AddNoteView;
+
 /**
  * Created by Taher on 30/05/2017.
  * Project: notes
  */
 
-public class AddNoteActivity {
+public class AddNoteActivity extends AppCompatActivity {
+
+    AddNotePresenter presenter;
+    AddNoteView view;
+    AddNoteModel model;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
