0796147225be5b43c192b97894ea4c39876002e8
Taher Haghverdi <t.haghverdi93@gmail.com>
2017-05-30 12:30:25 +0430

    AddNoteView base code

--- notes/app/src/main/java/ir/coursio/notes/view/AddNoteView.java
+++ notes/app/src/main/java/ir/coursio/notes/view/AddNoteView.java
@@ -1,9 +1,24 @@
 package ir.coursio.notes.view;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import ir.coursio.notes.R;
+
 /**
  * Created by Taher on 30/05/2017.
  * Project: notes
  */
 
-public class AddNoteView {
+@SuppressLint("ViewConstructor")
+public class AddNoteView extends FrameLayout {
+
+    public AddNoteView(@NonNull Activity activity) {
+        super(activity);
+        View view = inflate(getContext(), R.layout.activity_add_note, this);
+    }
+
 }
